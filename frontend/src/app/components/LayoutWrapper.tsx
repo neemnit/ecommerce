84f8803f901f0dfa820492f97db7dfabef4dfff6
Abhi <neemnit@gmail.com>
2025-03-16 20:28:41 +0530

    fixed private route

--- frontend/src/app/components/LayoutWrapper.tsx
+++ frontend/src/app/components/LayoutWrapper.tsx
@@ -1,11 +1,27 @@
+
 "use client";
 
-import { usePathname } from "next/navigation";
-import { ReactNode } from "react";
+
+import { useRouter, usePathname } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+import { useAppSelector } from "../hook";
 
 export default function LayoutWrapper({ children }: { children: ReactNode }) {
   const pathname = usePathname();
+  const router = useRouter();
+  const isAuthenticated = useAppSelector((state) => {
+    return state.user.isAuthenticated
+  }); // Adjust based on your Redux state
   const isHome = pathname === "/";
+
+  // List of private routes
+  const privateRoutes = ["/address", "/login-success", "/my-orders", "/order", "/payment", "/success"]; // Add your private routes
+
+  useEffect(() => {
+    if (!isAuthenticated && privateRoutes.includes(pathname)) {
+      router.push("/"); // Redirect to login if not authenticated
+    }
+  }, [isAuthenticated, pathname, router, privateRoutes]);
 
   return <div className={isHome ? "m-4" : ""}>{children}</div>;
 }
